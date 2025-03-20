9dc8a530d82284b49344bdbcf9d60504c35070c4
HERNANDIITO <pablohhgg@gmail.com>
2025-03-20 12:43:59 +0100

    refactorizacion

--- backend/schemas/user.schema.js
+++ backend/schemas/user.schema.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     displayName:    { type: String, required: true },
     email:          { type: String, required: true, unique: true },
-    password:       { type: String, required: true },
+    password:       { type: String, required: true }, // [7-15 chars], 1 simbolo, 1 minuscula, 1 mayuscula, 1 numero
     signupDate:     { type: Date,   required: true },
     lastLogin:      { type: Date,   required: true }
     
