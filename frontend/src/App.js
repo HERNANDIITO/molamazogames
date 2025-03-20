90f77cea4b1fdff38e534e552a23b04c82d3f47b
laCalvaRasta <donsilvajavier@gmail.com>
2025-03-20 11:30:28 +0100

    Registrarse (developing)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,34 @@ import { useEffect, useState } from 'react';
 import Button from './components/Button/Button';
 import { FaArrowRight } from 'react-icons/fa';  
 
-function App() {
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import Registrar from "./pages/Registrar/Registrar";
 
-  // Funncion de ejemplo, estaria mejor en un archivo separado
+function AppContent() {
+  const location = useLocation(); // Obtiene la ruta actual
+
+  // Función de ejemplo
   const handleClick = () => {
     alert('¡Botón clicado!');
   };
 
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-    
+        
+
+        <Routes>
+          <Route path="/registrar" element={<Registrar />} />
+        </Routes>
+
+        {/* Mostrar los botones solo si NO estamos en "/registrar" */}
+        {location.pathname !== "/registrar" && (   
+          <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+
         <a
           className=""
           href="https://reactjs.org"
@@ -29,81 +41,87 @@ function App() {
         >
           Learn React
         </a>
+            <Button
+              label="Next"
+              icon={<FaArrowRight />}
+              iconPosition="left"
+              onClick={handleClick}
+              className="seleccionable-btn"
+              href="#"
+            />
+            
+            <Button
+              label="Next"
+              icon={<FaArrowRight />}
+              iconPosition="alone"
+              onClick={handleClick}
+              className="enano-btn"
+              href="#"
+            />
 
+            <Button
+              label="Next"
+              icon={<FaArrowRight />}
+              iconPosition="left"
+              onClick={handleClick}
+              className="mediano-btn"
+              href="#"
+            />
 
-        <Button
-          label="Next"
-          icon={<FaArrowRight />}
-          iconPosition="left"
-          onClick={handleClick}
-          className="seleccionable-btn"
-          href="#"
-        />
+            <Button
+              label="Next"
+              icon={<FaArrowRight />}
+              iconPosition="left"
+              onClick={handleClick}
+              className="grande-btn"
+              href="#"
+            />
 
-        <Button
-          label="Next"
-          icon={<FaArrowRight />}
-          iconPosition="alone"
-          onClick={handleClick}
-          className="enano-btn"
-          href="#"
-        />
+            <Button
+              label="Next"
+              icon={<FaArrowRight />}
+              iconPosition="left"
+              onClick={handleClick}
+              className="enorme-btn secondary-btn"
+              href="#"
+            />
 
-        <Button
-          label="Next"
-          icon={<FaArrowRight />}
-          iconPosition="left"
-          onClick={handleClick}
-          className="mediano-btn"
-          href="#"
-        />
+            <Button
+              label="Next"
+              icon={<FaArrowRight />}
+              iconPosition="left"
+              onClick={handleClick}
+              className="secondary-btn active-btn"
+            />
 
-        <Button
-          label="Next"
-          icon={<FaArrowRight />}
-          iconPosition="left"
-          onClick={handleClick}
-          className="grande-btn"
-          href="#"
-        />
+            <Button
+              label="Next"
+              icon={<FaArrowRight />}
+              iconPosition="right"
+              onClick={handleClick}
+              className="active-btn"
+            />
 
-        <Button
-          label="Next"
-          icon={<FaArrowRight />}
-          iconPosition="left"
-          onClick={handleClick}
-          className="enorme-btn secondary-btn"
-          href="#"
-        />
-
-        <Button
-          label="Next"
-          icon={<FaArrowRight />}
-          iconPosition="left"
-          onClick={handleClick}
-          className="secondary-btn active-btn"
-        />
-
-        <Button
-          label="Next"
-          icon={<FaArrowRight />}
-          iconPosition="right"
-          onClick={handleClick}
-          className="active-btn"
-        />
-
-        
-        <Button
-          label="Next"
-          icon={<FaArrowRight />}
-          iconPosition="left"
-          onClick={handleClick}
-          disabled="true"
-          className="seleccionable-btn"
-        />
-    
+            <Button
+              label="Next"
+              icon={<FaArrowRight />}
+              iconPosition="left"
+              onClick={handleClick}
+              disabled={true}
+              className="seleccionable-btn"
+            />
+          </>
+        )}
       </header>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Registrar/Registrar.js
@@ -0,0 +1,65 @@
+import './Registrar.scss';
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
+import Button from './components/Button/Button';
+
+const Registrar = () => {
+  const [formData, setFormData] = useState({
+      name: "",
+      email: "",
+      phone: "",
+      password: "",
+      reppass: "",
+    });
+  
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log("Datos enviados:", formData);
+    };
+
+  return (
+    <div className="register-page">
+      <form onSubmit={handleSubmit} className="register-form">
+            <p className="title">
+              Registrarse
+            </p>
+            <hr />
+            <p className="aviso">
+              Los campos obligatorios están marcados con *
+            </p>
+            <label>
+              Nombre de usuario*<br/>
+              <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+            </label>
+            <label>
+              Email*<br/>
+              <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+            </label>
+            <label>
+              Teléfono<br/>
+              <input type="tel" name="phone" value={formData.phone} onChange={handleChange} />
+            </label>
+            <label>
+              Contraseña*  
+              <div className="input-container">
+                <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+                <FontAwesomeIcon icon={faCircleInfo} className="info-icon" />
+              </div>
+            </label>
+      
+            <label>
+              Confirmar contraseña*<br/>
+              <input type="password" name="reppass" value={formData.reppass} onChange={handleChange} required />
+            </label>
+            <button type="submit">Registrarse</button>
+          </form>
+    </div>
+  );
+};
+
+export default Registrar;
