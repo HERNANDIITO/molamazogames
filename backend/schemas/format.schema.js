e2eb8737388679dfc3e953b313ddf55d3eebfd13
phg21 <pablohhgg@gmail.com>
2025-04-25 19:08:46 +0200

    ajuste de importaciones

--- backend/schemas/format.schema.js
+++ backend/schemas/format.schema.js
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
 const formatSchema = new mongoose.Schema({
     name: { 
--- backend/schemas/like.schema.js
+++ backend/schemas/like.schema.js
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
 const likeSchema = new mongoose.Schema({
     author: {
